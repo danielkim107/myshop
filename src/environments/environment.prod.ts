21c49dc447b3a7bd20c0125664e6f284738d23e1
Daniel Kim <danielkim107@gmail.com>
2020-10-05 17:10:41 +0900

    First commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+	apiKey: "AIzaSyDALew2HvGPQ99Y4DPFlN0Y0h3PaSAUr3s",
+    authDomain: "myshop-ab3ac.firebaseapp.com",
+    databaseURL: "https://myshop-ab3ac.firebaseio.com",
+    projectId: "myshop-ab3ac",
+    storageBucket: "myshop-ab3ac.appspot.com",
+    messagingSenderId: "1080580665260",
+    appId: "1:1080580665260:web:65ecdf8b8931ce3cb85409",
+    measurementId: "G-VCT605R01N"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+	apiKey: "AIzaSyDALew2HvGPQ99Y4DPFlN0Y0h3PaSAUr3s",
+    authDomain: "myshop-ab3ac.firebaseapp.com",
+    databaseURL: "https://myshop-ab3ac.firebaseio.com",
+    projectId: "myshop-ab3ac",
+    storageBucket: "myshop-ab3ac.appspot.com",
+    messagingSenderId: "1080580665260",
+    appId: "1:1080580665260:web:65ecdf8b8931ce3cb85409",
+    measurementId: "G-VCT605R01N"
+  }
 };
 
 /*
